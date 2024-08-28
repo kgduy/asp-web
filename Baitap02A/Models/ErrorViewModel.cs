b70071fb8a25b370c1b8418bd5f9d8b9322ab0f4
kgduy <phungkhanhduy2362@gmail.com>
2024-08-28 15:00:01 +0700

    Hoan thanh

---
+++ Baitap02A/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Baitap02A.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
