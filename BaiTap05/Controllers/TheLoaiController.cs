67bebc3e1756422c37c831b59fc716a87f0ec628
Phùng Khánh Duy <phungkhanhduy2362@gmail.com>
2024-09-05 22:23:36 +0700

    hoan thanh

---
+++ BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,25 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 29";
+            ViewBag.Thang = "Tháng 07";
+            ViewData["Nam"] = "Năm 2004"; 
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh thám"
+            };
+            return View(theloai);
+        }
+    }
+}
