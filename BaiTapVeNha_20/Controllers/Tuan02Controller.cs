31871c030bc454759beb635b9a51b9907c2d537a
Phùng Khánh Duy <phungkhanhduy2362@gmail.com>
2024-09-05 22:24:19 +0700

    hoan thanh

---
+++ BaiTapVeNha_20/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha_20.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Hoten = "Phùng Khánh Duy";
+            ViewBag.MSSV = "1822041439";
+            ViewData["Nam"] = "2004";
+            return View();
+        }
+    }
+}
