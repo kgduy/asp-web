0b111a7bb89aa8a84a1076547425c8a1cde1afc5
Phùng Khánh Duy <phungkhanhduy2362@gmail.com>
2024-09-11 15:40:23 +0700

    Hoang Thanh

---
+++ Baitap06/Controllers/TaiKhoanController.cs
@@ -0,0 +1,21 @@
+﻿using Baitap06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap06.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taiKhoan)
+        {
+            if (taiKhoan != null && taiKhoan.Password != null && taiKhoan.Password.Length > 0)
+            {
+                taiKhoan.Password = taiKhoan.Password + "_da_ma_hoa";
+            }
+            return View(taiKhoan);
+        }
+    }
+}
