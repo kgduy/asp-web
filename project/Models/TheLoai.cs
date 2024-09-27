6c6b6b83c688ad1be02ab97cedd5e619afb9a066
Phùng Khánh Duy <phungkhanhduy2362@gmail.com>
2024-09-27 22:54:05 +0700

    25/09

---
+++ project/Models/TheLoai.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage="Không được để trống Tên thể loại!")]
+        [Display(Name="Thể loại")]
+        public string Name { get; set; }
+        [Required(ErrorMessage="Không đúng định dạng ngày!")]
+        [Display(Name="Ngày Tạo")]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
