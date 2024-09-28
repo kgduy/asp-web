54e3da4a310b5157ef45fe342e455feab2696b93
Phùng Khánh Duy <phungkhanhduy2362@gmail.com>
2024-09-28 15:49:21 +0700

    BaiKiemTra02

---
+++ BaiKiemTra02/BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Không được để trống Tên lớp học!")]
+        [Display(Name ="Lớp Học")]
+        public string TenLopHoc { get; set; }
+        [Required(ErrorMessage = "Không đúng định dạng ngày!")]
+        [Display(Name = "Năm Nhập Học")]
+        public DateTime NamNhapHoc { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Không đúng định dạng ngày!")]
+        [Display(Name = "Năm ra trường")]
+        public DateTime NamRaTruong { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Không được để trống")]
+        [Display(Name = "Số Lượng Sinh Viên")]
+        public int SoLuongSinhVien { get; set; }
+    }
+}
