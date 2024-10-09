e9918790dc5d44bbe95021969370835b5d67a074
Phùng Khánh Duy <phungkhanhduy2362@gmail.com>
2024-10-09 15:00:43 +0700

    Nop Bai

---
+++ BaiKiemTra03_03/BaiKiemTra03_03/Controllers/ContractController.cs
@@ -0,0 +1,60 @@
+﻿
+using BaiKiemTra03_03.Data;
+using BaiKiemTra03_03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra03_03.Controllers
+{
+    public class ContractController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ContractController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var contract = _db.Contract.ToList();
+            ViewBag.Contract = contract;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Contract ct)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Contract.Add(ct);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var ct = _db.Contract.Find(id);
+            return View(ct);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Contract ct)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Contract.Update(ct);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ BaiKiemTra03_03/BaiKiemTra03_03/Models/Contract.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaiKiemTra03_03.Models
+{
+    public class Contract
+    {
+        [Key]
+        public int Contract_Id { get; set; }
+        [Required]
+        public string Contract_name { get; set; }
+        [Required]
+        public DateTime Signing_Date { get; set; }
+        public decimal Contract_Value { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        [ValidateNever]
+        public Customer Customer { get; set; }
+    }
+}
